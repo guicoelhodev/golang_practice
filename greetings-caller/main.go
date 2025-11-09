6d6d934c5e8f3fc01ed0ffae1fac6aec487948d6
guicoelhodev <gs.coelho_dev@outlook.com>
2025-11-09 15:19:24 -0300

    feat: create for loop to test allocation capacity on slice

--- greetings-caller/main.go
+++ greetings-caller/main.go
@@ -1,20 +1,38 @@
 package main
 
-import(
+import (
 	"fmt"
-	"log"
-	"example/greetings"
+	"time"
+	//"example/greetings"
+	//"log"
 )
 
 func main(){
-	message, err := greetings.Hello("Guilherme")
-
-	log.SetPrefix("greetings: ")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(message)
+	loop()
 }
 
+func loop() {
+	start := time.Now()
+
+	items := make([]int, 0, 1000000)
+
+	for i := 0; i < 1000000; i++ {
+		items = append(items, i)
+	}
+
+	elapsed := time.Since(start)
+
+	fmt.Println("Time taken:", elapsed)  // Time taken: 1.827185ms
+}
+
+//func greetingFn(){
+//	message, err := greetings.Hello("Guilherme")
+//
+//	log.SetPrefix("greetings: ")
+//
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	fmt.Println(message)
+//}
